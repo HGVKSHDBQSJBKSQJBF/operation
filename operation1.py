d3a8dbc372001c378d64a9569acded83a67e7b5f
lecapitainecoeur <rugbya13grans@gmail.com>
2025-02-15 08:53:14 +0100

    Update operation1.py

--- operation1.py
+++ operation1.py
@@ -15,6 +15,7 @@ class op(Enum):
     puissance2 = 5
     racinecarré = 6
     puissance3 = 7
+    modulo = 8
 
 
 class cal(Enum):
@@ -25,6 +26,7 @@ class cal(Enum):
     puissance2 = 5
     racinecarré = 6
     puissance3 = 7
+    modulo = 8
 
 
 def calculette():
@@ -40,6 +42,7 @@ def calculette():
             or operation == cal.soustraction
             or operation == cal.multiplication
             or operation == cal.division
+            or operation == cal.modulo
         ):
             entry2 = Entry(calcul)
             entry2.place(x=0, y=150)
@@ -80,6 +83,9 @@ def calculette():
             elif operation == cal.puissance3:
                 solution = nombre_1 * nombre_1 * nombre_1
                 calcul.title("puissance3")
+            elif operation == cal.modulo:
+                solution = nombre_1 % nombre_2
+                calcul.title("modulo")
 
         def deux_fonction():
             entry_get()
@@ -88,6 +94,7 @@ def calculette():
                 or operation == cal.soustraction
                 or operation == cal.multiplication
                 or operation == cal.division
+                or operation == cal.modulo
             ):
                 entry_get2()
             valider()
@@ -104,6 +111,7 @@ def calculette():
             or operation == cal.soustraction
             or operation == cal.multiplication
             or operation == cal.division
+            or operation == cal.modulo
         ):
             label1 = Label(
                 calcul,
@@ -155,46 +163,63 @@ def calculette():
     def multiplicationcal():
         solution1(cal.multiplication)
 
+    def modulocal():
+        solution1(cal.modulo)
+
     fenetrecalcul = Toplevel()
     fenetrecalcul.title("calculette")
-    fenetrecalcul.geometry("500x350")
+    fenetrecalcul.geometry("1110x300")
     fenetrecalcul.configure(bg="gray")
+    f = Font(size=20)
 
     label2 = Label(
-        fenetrecalcul, text="choisissez une operation", font=("Comic sans ms", 30)
+        fenetrecalcul, text="choisissez une operation:", font=("Comic sans ms", 30)
     )
     label2.place(x=0, y=0)
     label2.configure(bg="gray")
-    buton1 = Button(fenetrecalcul, text="multiplication", command=multiplicationcal)
-    buton1.place(x=0, y=100)
-    buton2 = Button(fenetrecalcul, text="division", command=divisioncal)
-    buton2.place(x=100, y=100)
-    buton3 = Button(fenetrecalcul, text="addition", command=additioncal)
-    buton3.place(x=200, y=100)
-    buton5 = Button(fenetrecalcul, text="soustraction", command=soustractioncal)
-    buton5.place(x=300, y=100)
-    buton6 = Button(fenetrecalcul, text="puissance2", command=puissance2cal)
-    buton6.place(x=400, y=100)
-    buton7 = Button(fenetrecalcul, text="puissance3", command=puissance3cal)
-    buton7.place(x=0, y=200)
-    buton8 = Button(fenetrecalcul, text="racine carré", command=racinecarrécal)
-    buton8.place(x=100, y=200)
-    buton4 = Button(fenetrecalcul, text="a venir")
-    buton4.place(x=200, y=200)
-    buton9 = Button(fenetrecalcul, text="a venir")
-    buton9.place(x=300, y=200)
-    buton10 = Button(fenetrecalcul, text="a venir")
-    buton10.place(x=400, y=200)
-    buton11 = Button(fenetrecalcul, text="a venir")
-    buton11.place(x=0, y=300)
-    buton12 = Button(fenetrecalcul, text="a venir")
-    buton12.place(x=100, y=300)
-    buton14 = Button(fenetrecalcul, text="a venir")
-    buton14.place(x=300, y=300)
-    buton15 = Button(fenetrecalcul, text="a venir")
-    buton15.place(x=400, y=300)
-    fermer = Button(fenetrecalcul, text="fermer", command=fenetrecalcul.destroy)
-    fermer.place(x=200, y=300)
+    width = 12
+    buton1 = Button(
+        fenetrecalcul, text="multiplication", command=multiplicationcal, width=width
+    )
+    buton2 = Button(fenetrecalcul, text="division", command=divisioncal, width=width)
+    buton3 = Button(fenetrecalcul, text="addition", command=additioncal, width=width)
+    buton5 = Button(
+        fenetrecalcul, text="soustraction", command=soustractioncal, width=width
+    )
+    buton6 = Button(
+        fenetrecalcul, text="puissance2", command=puissance2cal, width=width
+    )
+    buton7 = Button(
+        fenetrecalcul, text="puissance3", command=puissance3cal, width=width
+    )
+    buton8 = Button(
+        fenetrecalcul, text="racine carré", command=racinecarrécal, width=width
+    )
+    buton4 = Button(fenetrecalcul, text="modulo", command=modulocal, width=width)
+    buton9 = Button(fenetrecalcul, text="a venir", width=width)
+    buton10 = Button(fenetrecalcul, text="a venir", width=width)
+    buton11 = Button(fenetrecalcul, text="a venir", width=width)
+    buton12 = Button(fenetrecalcul, text="a venir", width=width)
+    buton14 = Button(fenetrecalcul, text="a venir", width=width)
+    buton15 = Button(fenetrecalcul, text="a venir", width=width)
+    fermer = Button(
+        fenetrecalcul, text="fermer", command=fenetrecalcul.destroy, width=width
+    )
+    buton1.grid(row=1, column=0, pady=(75, 10), padx=10)
+    buton2.grid(row=1, column=1, pady=(75, 10), padx=10)
+    buton3.grid(row=1, column=2, pady=(75, 10), padx=10)
+    buton4.grid(row=1, column=3, pady=(75, 10), padx=10)
+    buton5.grid(row=1, column=4, pady=(75, 10), padx=10)
+    buton6.grid(row=2, column=0, pady=10, padx=10)
+    buton7.grid(row=2, column=1, pady=10, padx=10)
+    buton8.grid(row=2, column=2, pady=10, padx=10)
+    buton9.grid(row=2, column=3, pady=10, padx=10)
+    buton10.grid(row=2, column=4, pady=10, padx=10)
+    buton11.grid(row=3, column=0, pady=10, padx=10)
+    buton12.grid(row=3, column=1, pady=10, padx=10)
+    fermer.grid(row=3, column=2, pady=10, padx=10)
+    buton14.grid(row=3, column=3, pady=10, padx=10)
+    buton15.grid(row=3, column=4, pady=10, padx=10)
     buton1.configure(bg="gray")
     buton2.configure(bg="gray")
     buton3.configure(bg="gray")
@@ -210,6 +235,22 @@ def calculette():
     buton14.configure(bg="gray")
     buton15.configure(bg="gray")
     fermer.configure(bg="gray")
+
+    buton1["font"] = f
+    buton2["font"] = f
+    buton3["font"] = f
+    buton4["font"] = f
+    buton5["font"] = f
+    buton6["font"] = f
+    buton7["font"] = f
+    buton8["font"] = f
+    buton9["font"] = f
+    buton10["font"] = f
+    buton11["font"] = f
+    buton12["font"] = f
+    fermer["font"] = f
+    buton14["font"] = f
+    buton15["font"] = f
 
 
 def valider_pseudo():
@@ -249,10 +290,10 @@ def open_mail(event=None):
     subject = "idée pour votre projet operation"
     body = "Bonjour,\n\n"
 
-    # Lien Gmail avec adresse, sujet et corps de message pré-remplis
+    #  Lien Gmail avec adresse, sujet et corps de message pré-remplis
     gmail_link = f"https://mail.google.com/mail/?view=cm&fs=1&to={email}&su={subject}&body={body}"
 
-    # Ouvrir Gmail dans le navigateur
+    #  Ouvrir Gmail dans le navigateur
     webbrowser.open(gmail_link)
 
 
@@ -265,6 +306,7 @@ def credit():
     credit = Toplevel()
     credit.geometry("600x400")
     credit.title(f"credits")
+    credit.configure(bg="gray")
     chaine = f"programeur: ☺lecapitainecoeur☺"
     label = Label(
         credit,
@@ -272,6 +314,7 @@ def credit():
         fg="red",
         font=("Comic sans ms", 29),
     )
+    label.configure(bg="gray")
     label.place(x=0, y=0)
     chaine1 = f"créateur: ☺lecapitainecoeur☺"
     label1 = Label(
@@ -281,6 +324,7 @@ def credit():
         font=("Comic sans ms", 29),
     )
     label1.place(x=0, y=100)
+    label1.configure(bg="gray")
     chaine2 = f"imaginateur: ☺lecapitainecoeur☺"
     label2 = Label(
         credit,
@@ -289,30 +333,37 @@ def credit():
         font=("Comic sans ms", 29),
     )
     label2.place(x=0, y=200)
-    buton = Button(credit, text="retour", command=credit.destroy)
+    label2.configure(bg="gray")
+    buton = Button(credit, text="retour", fg="red", command=credit.destroy)
     buton["font"] = f
     buton.place(x=250, y=325)
+    buton.configure(bg="gray")
 
 
 def afficher_historique():
-    try:
-        with open("scores.txt", "r") as fichier:
-            contenu = fichier.read()
-    except FileNotFoundError:
-        contenu = "Aucun historique disponible pour le moment."
+    with open("scores.txt", "r") as fichier:
+        contenu = fichier.read()
+        if contenu == "":
+            contenu = "Il n'y a pas d'historique pour le moment"
+        lignes = contenu.splitlines()
+        nombre_de_lignes = len(lignes)
+        if nombre_de_lignes >= 40:
+            lignes_supprimees = lignes[-20]
+            with open("scores.txt", "w") as fichier:
+                fichier.writelines(lignes_supprimees)
+            with open("scores.txt", "r") as fichier:
+                contenu = fichier.read()
 
-    # Créer une fenêtre pour afficher l'historique
+    #  Créer une fenêtre pour afficher l'historique
     historique = Toplevel()
     historique.title("Historique des Scores")
-    historique.geometry("950x500")
+    historique.geometry("1000x500")
 
-    # Zone de texte pour afficher l'historique
+    #  Zone de texte pour afficher l'historique
     texte = Text(historique, wrap="word", font=("Comic sans ms", 14))
     texte.insert("1.0", contenu)
     texte.config(state="disabled")  # Empêcher la modification du texte
     texte.pack(expand=True, fill="both")
-
-    # Bouton de fermeture
 
 
 def enregistrer_score(user_reponse, solution, score, chaine2):
@@ -320,11 +371,11 @@ def enregistrer_score(user_reponse, solution, score, chaine2):
     with open("scores.txt", "a") as fichier:
         if user_reponse == solution:
             fichier.write(
-                f"{now} | {pseudo} | Réponse correcte | Score: {score} | operation: {chaine2}\n"
+                f"{now} | {pseudo} | Réponse correcte | Score: {score} | operation: {chaine2} {user_reponse}\n"
             )
         else:
             fichier.write(
-                f"{now} | {pseudo} | Réponse incorrecte | Score: {score}| operation: {chaine2}\n"
+                f"{now} | {pseudo} | Réponse incorrecte | Score: {score}| operation: {chaine2} {user_reponse}\n"
             )
 
 
@@ -336,6 +387,11 @@ def genererop(operation, alea=False):
         nombre_1 = random.randint(1, 11)
         chaine2 = f"{nombre_1}² = ?"
         root.title("puissance2")
+    elif operation == op.modulo:
+        nombre_1 = random.randint(10, 100)
+        nombre_2 = random.randint(1, 10)
+        chaine2 = f"{nombre_1} modulo {nombre_2} = ?"
+        root.title("modulo")
     elif operation == op.racinecarré:
         nombre_1 = random.randint(1, 11)
         chaine2 = f"racine carré de {nombre_1 * nombre_1} = ?"
@@ -421,6 +477,10 @@ def get_text(root, entry, operation, nombre_1, nombre_2=0, alea=False):
         solution = nombre_2 * nombre_1
         recommencer = multiplication
         chaine2 = f"{nombre_1} x {nombre_2} = ?"
+    elif operation == op.modulo:
+        solution = nombre_1 % nombre_2
+        recommencer = modulo
+        chaine2 = f"{nombre_1} modulo {nombre_2} = ?"
     elif operation == op.addition:
         solution = nombre_1 + nombre_2
         recommencer = addition
@@ -513,7 +573,7 @@ def get_text(root, entry, operation, nombre_1, nombre_2=0, alea=False):
         score += 1
         juste = Label(
             top,
-            text="cette reponse est juste",
+            text=f"cette reponse est juste",
             font=("Helvetica 14 bold"),
         )
         juste.grid(row=1, column=1)
@@ -529,7 +589,7 @@ def get_text(root, entry, operation, nombre_1, nombre_2=0, alea=False):
         scoreprint = Label(top, text=chainescore, font=("Comic Sans MS", 24))
         scoreprint.grid(row=2, column=1)
         scoreprint.configure(bg="gray")
-        chainesolution = "cette reponse est fausse", "solution:", solution
+        chainesolution = f"cette reponse est fausse, solution:, {solution}"
         faux = Label(
             top,
             text=chainesolution,
@@ -537,7 +597,6 @@ def get_text(root, entry, operation, nombre_1, nombre_2=0, alea=False):
         )
         faux.grid(row=1, column=1)
         faux.configure(bg="gray")
-        enregistrer_score(user_reponse, solution, score, chaine2)
 
     top.bind("<BackSpace>", lambda event: done())
     top.bind("<Return>", lambda event: again())
@@ -546,6 +605,7 @@ def get_text(root, entry, operation, nombre_1, nombre_2=0, alea=False):
 
 
 def puissance2_3():
+
     def puissance32():
         puissance2ou3.destroy()
         puissance3()
@@ -555,18 +615,28 @@ def puissance2_3():
         puissance2()
 
     puissance2ou3 = Toplevel()
+    puissance2ou3.configure(bg="gray")
     puissance2ou3.title(f"²ou³")
     puissance2ou3.geometry("400x200")
     label = Label(puissance2ou3, text="p²ou³", font=("Comic sans ms", 30))
     label.place(x=0, y=0)
+    label.configure(bg="gray")
     boton = Button(puissance2ou3, text="puissance³", command=puissance32)
-    boton.place(x=100, y=100)
+    boton.place(x=0, y=100)
+    boton["font"] = f
+    boton.configure(bg="gray")
     boton1 = Button(puissance2ou3, text="puissance²", command=puissance23)
-    boton1.place(x=300, y=100)
+    boton1.place(x=200, y=100)
+    boton1.configure(bg="gray")
+    boton1["font"] = f
 
 
 def puissance3():
     genererop(op.puissance3)
+
+
+def modulo():
+    genererop(op.modulo)
 
 
 def puissance2():
@@ -605,6 +675,7 @@ f = Font(size=20)
 fenetre_pseudo.title("Connexion")
 fenetre_pseudo.geometry("400x300")
 fenetre_pseudo.configure(bg="gray")
+fenetre_pseudo.focus()
 
 labelpseudo = Label(
     fenetre_pseudo, text="Entrez votre pseudo :", font=("Comic sans ms", 20)
@@ -622,7 +693,7 @@ entry_pseudo.focus_set()
 bouton13 = Button(
     fenetre_pseudo, text="fermer", font=("Comic sans ms", 16), command=destroie, width=6
 )
-bouton13.place(x=150, y=200)
+bouton13.pack(pady=(10, 0))
 bouton13["font"] = f
 bouton13.configure(bg="gray")
 
@@ -630,18 +701,20 @@ bouton_valider = Button(
     fenetre_pseudo,
     text="Valider",
     command=deux_fonction,
-    font=("Comic sans ms", 14),
+    font=("Comic sans ms", 16),
 )
-bouton_valider.place(x=150, y=250)
+bouton_valider.pack(pady=0)
 bouton_valider.configure(bg="gray")
 fenetre_pseudo.bind("<Return>", lambda event: valider_pseudo())
 pseudo = ""
 
 fenetre_pseudo.grab_set()
 fenetre_pseudo.mainloop()
+fenetre_pseudo.mainloop()
 
-if pseudo != None:
+if pseudo != "":
     base = Tk()
+    base.focus()
     f = Font(size=20)
     base.configure(bg="gray")
     base.title("operation")
@@ -650,10 +723,10 @@ if pseudo != None:
         text="choisissez une opération:",
         font=("Comic sans ms", 30),
     )
-    basefortext.grid(row=0, column=2)
+    basefortext.place(x=0, y=0)
     basefortext.configure(bg="gray")
 
-    base.geometry("1000x600")
+    base.geometry("900x450")
 
     width = 12
     bouton1 = Button(base, text="multiplication", command=multiplication, width=width)
@@ -663,11 +736,7 @@ if pseudo != None:
     bouton5 = Button(base, text="aléatoire", command=aleatoire, width=width)
     bouton6 = Button(base, text="racine carré", command=racinecarré, width=width)
     bouton7 = Button(base, text="puissance2 ou 3", command=puissance2_3, width=width)
-    bouton8 = Button(
-        base,
-        text="a venir",
-        width=width,
-    )
+    bouton8 = Button(base, text="modulos", width=width, command=modulo)
     bouton9 = Button(
         base,
         text="a venir",
@@ -685,8 +754,16 @@ if pseudo != None:
     )
     bouton11 = Button(base, text="calculette", width=width, command=calculette)
     bouton12 = Button(
-        base, text="historique score", width=width, command=afficher_historique
+        base,
+        text="historique score",
+        width=width,
+        command=afficher_historique,
+        fg="red",
     )
+    bouton13 = Button(base, text="fermer", fg="red", command=base.destroy, width=width)
+    bouton14 = Button(base, text="support", command=open_mail, fg="red", width=width)
+    bouton15 = Button(base, text="github", command=ouvrir_github, fg="red", width=width)
+    createur = Button(base, text="credits", fg="red", command=credit, width=width)
 
     bouton1.configure(bg="gray")
     bouton2.configure(bg="gray")
@@ -701,20 +778,28 @@ if pseudo != None:
     bouton11.configure(bg="gray")
     bouton12.configure(bg="gray")
     bouton0.configure(bg="gray")
+    bouton13.configure(bg="gray")
+    bouton14.configure(bg="gray")
+    bouton15.configure(bg="gray")
+    createur.configure(bg="gray")
 
-    bouton1.place(x=0, y=100)
-    bouton2.place(x=250, y=100)
-    bouton3.place(x=500, y=100)
-    bouton4.place(x=750, y=100)
-    bouton5.place(x=0, y=200)
-    bouton6.place(x=250, y=200)
-    bouton7.place(x=500, y=200)
-    bouton8.place(x=750, y=200)
-    bouton9.place(x=0, y=300)
-    bouton0.place(x=250, y=300)
-    bouton10.place(x=500, y=300)
-    bouton11.place(x=750, y=300)
-    bouton12.place(x=750, y=400)
+    bouton1.grid(row=1, column=0, pady=(75, 10), padx=10)
+    bouton2.grid(row=1, column=1, pady=(75, 10), padx=10)
+    bouton3.grid(row=1, column=2, pady=(75, 10), padx=10)
+    bouton4.grid(row=1, column=3, pady=(75, 10), padx=10)
+    bouton5.grid(row=2, column=0, pady=10, padx=10)
+    bouton6.grid(row=2, column=1, pady=10, padx=10)
+    bouton7.grid(row=2, column=2, pady=10, padx=10)
+    bouton8.grid(row=2, column=3, pady=10, padx=10)
+    bouton9.grid(row=3, column=0, pady=10, padx=10)
+    bouton0.grid(row=3, column=1, pady=10, padx=10)
+    bouton10.grid(row=3, column=2, pady=10, padx=10)
+    bouton11.grid(row=3, column=3, pady=10, padx=10)
+    bouton12.grid(row=4, column=3, pady=10, padx=10)
+    bouton13.grid(row=5, column=1, columnspan=2, pady=10, padx=10)
+    bouton14.grid(row=4, column=2, pady=10, padx=10)
+    bouton15.grid(row=4, column=1, pady=10, padx=10)
+    createur.grid(row=4, column=0, pady=10, padx=10)
 
     bouton1["font"] = f
     bouton2["font"] = f
@@ -729,25 +814,9 @@ if pseudo != None:
     bouton10["font"] = f
     bouton11["font"] = f
     bouton12["font"] = f
-
-    bouton13 = Button(base, text="fermer", command=base.destroy, width=10)
-    bouton13.place(x=400, y=500)
     bouton13["font"] = f
-    bouton13.configure(bg="gray")
-
-    bouton14 = Button(base, text="support", command=open_mail, fg="red", width=10)
-    bouton14.place(x=0, y=400)
     bouton14["font"] = f
-    bouton14.configure(bg="gray")
-
-    bouton15 = Button(base, text="github", command=ouvrir_github, fg="red", width=10)
-    bouton15.place(x=250, y=400)
     bouton15["font"] = f
-    bouton15.configure(bg="gray")
-
-    createur = Button(base, text="credits", command=credit, width=10)
-    createur.place(x=500, y=400)
     createur["font"] = f
-    createur.configure(bg="gray")
 
     base.mainloop()
